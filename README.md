e8e159c74c28ae4f6954f3d58af21aa791e3c6da
“SayyedulBappy” <msbappy.msb@gmail.com>
2020-01-09 22:30:49 +0600

    initial project done

---
+++ README.md
@@ -0,0 +1,21 @@
+# Blog Application
+
+A blog application made with python django framework
+
+## Features
+
+- user authentication and authorization
+- CRUD functionality for posts
+- comment system on posts
+
+## Description
+
+Users can create account and add new posts to the site and update and delete their posts. Users can add comments to any post and delete comments
+
+## What did I learn
+
+- Basics of Django Framework
+- Creating Models with Django ORM
+- Using Function Based Views and Class Based Views
+- url routing and template rendering
+- user authentication and authorization
---
+++ blog/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from .views import (
+    PostListView,
+
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+
+
+)
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
+    path('post/<int:pk1>/comments/<int:pk2>/delete', views.comment_delete, name='comment-delete'),
+
+]
